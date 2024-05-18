3586f4cb7e6711b3377a716fdfde1a78b5a2ae11
kalharko <46170581+kalharko@users.noreply.github.com>
2024-05-18 15:48:47 +0200

    :sparkles: Instance sushi, sushi move, punish speed

---
+++ Assets/Scripts/Sushi/SushiBehavior.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class SushiBehavior : MonoBehaviour
+{
+
+    // Référence à la spline
+    private SplineContainer spline;
+
+    // Vitesse du sushi
+    public float speed = 1.0f;
+
+    // Position du sushi sur la spline
+    private float positionOnSpline = 0.0f;
+    private bool following_spline = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // On récupère la référence vers la spline
+        spline = GetComponentInParent<SplineContainer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On fait avancer le sushi
+        if (following_spline)
+        {
+            positionOnSpline += speed * Time.deltaTime;
+            transform.position = spline.EvaluatePosition(positionOnSpline);
+        }
+    }
+
+    void StopFollowingSpline()
+    {
+        following_spline = false;
+    }
+
+    void SelectApearance() {
+        // TODO
+    }
