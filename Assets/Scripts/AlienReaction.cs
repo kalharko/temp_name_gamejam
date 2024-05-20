7d2fef8bfcc7a6d42d206f5daa8d2b582e72dc0d
hermance <hermance.dreano@utbm.fr>
2024-05-20 13:04:37 +0200

    alien sprite animation

--- Assets/Scripts/AlienReaction.cs
+++ Assets/Scripts/AlienReaction.cs
@@ -1,5 +1,7 @@
 using System;
 using UnityEngine;
+using System.Collections;
+using Unity.VisualScripting;
 
 public class AngryAlien : MonoBehaviour
 {
@@ -13,8 +15,15 @@ public class AngryAlien : MonoBehaviour
     public AudioSource happyAudioSource;
     public AudioClip happyAudioClip;
 
+    //animate alien behavior
+    public SpriteRenderer spriteRenderer;
+    public Sprite spriteHappy;
+    public Sprite spriteAngry;
+    public Sprite spriteNormal;
+
     private void Awake()
     {
+        spriteRenderer.sprite = spriteNormal;
         GameManager.OnGameStateChanged += OnGameStateChanged;
         animator = GetComponent<Animator>();
     }
@@ -28,17 +37,41 @@ public class AngryAlien : MonoBehaviour
     {
         if (state == GameState.HasFailed)
         {
+            spriteRenderer.sprite = spriteAngry;
+            //StartCoroutine(AlienBehaviorSprite(spriteAngry, spriteNormal));
             animator.SetTrigger(HasFailed);
             angryAudioSource.time = 0;
             angryAudioSource.PlayOneShot(angryAudioClip);
+            Invoke(nameof(ResetSprite), 1f);
             
         } 
         else if (state == GameState.HasSucceeded)
         {
+
+            //StartCoroutine(AlienBehaviorSprite(spriteAngry, spriteNormal));
+            spriteRenderer.sprite = spriteHappy;
             animator.SetTrigger(HasSucceeded);
             happyAudioSource.time = 0;
             happyAudioSource.PlayOneShot(happyAudioClip);
+            Invoke(nameof(ResetSprite), 1f);
             
         }
+        
     }
+
+    private void ResetSprite()
+    {
+        spriteRenderer.sprite = spriteNormal;
+    }
+
+    /*IEnumerator AlienBehaviorSprite(Sprite sprite1, Sprite sprite2)
+    {
+        while (true)
+        {
+            spriteRenderer.sprite = sprite1;
+            yield return new WaitForSeconds(1f);
+            spriteRenderer.sprite = sprite2;
+        }
+    }*/
+
 }
